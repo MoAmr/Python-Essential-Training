c153867e094d382a23e0de6ddb07f1fa36330180
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-01-11 01:47:38 +0200

    Overview of type. closes #13

---
+++ Types and Values/Types.py
@@ -0,0 +1,18 @@
+# types in Python.
+# type is a built-in function that returns the type of the value
+
+var1 = 7
+var2 = 7.0
+var3 = '7.0'
+var4 = True
+var5 = None
+print('var1 is {}'.format(var1))
+print('var2 is {}'.format(var2))
+print('var3 is {}'.format(var3))
+print('var4 is {}'.format(var4))
+print('var5 is {}'.format(var5))
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+print(type(var5))
