6bc4ba769193a097e37e6909d14b1fe1a2627bc8
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-01-10 23:01:03 +0200

    Blocks and Scopes. closes #3

---
+++ Language Overview/Blocks.py
@@ -0,0 +1,13 @@
+# Blocks and Scope
+
+x = 42
+y = 73
+
+if x < y:
+    z = 112
+    print('x < y: x is {} and y is {}'.format(x, y))
+    print('line 2')
+    print('line 3')
+    print('line 4')
+
+print('z is {}'.format(z))
