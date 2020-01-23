af5928a4acbd96a2a3c813e1bf1553a8455c2b49
mohamed amr <mohamed.amr@asset.com.eg>
2020-01-23 18:13:13 +0200

    Arithmetic Operators. closes #29

---
+++ Operators/Arithmetic Operators.py
@@ -0,0 +1,18 @@
+# Arithmetic Operators in Python
+
+x = 5
+y = 3
+r1 = x + y
+r2 = x - y
+r3 = x * y
+r4 = x / y
+r5 = x // y
+r6 = x % y
+r7 = -x
+print(f'result is {r1}')
+print(f'result is {r2}')
+print(f'result is {r3}')
+print(f'result is {r4}')
+print(f'result is {r5}')
+print(f'result is {r6}')
+print(f'result is {r7}')
