023487af46d8c23e975af9a4c78757e97f566328
mohamed amr <mohamed.amr@asset.com.eg>
2020-01-23 17:25:28 +0200

    Conditional syntax in Python. closes #25

---
+++ Conditionals/Conditional syntax.py
@@ -0,0 +1,18 @@
+# Conditional syntax in Python.
+
+x = 5
+
+if x == 0:
+    print('0 is true')
+elif x == 1:
+    print('1 is true')
+elif x == 2:
+    print('2 is false')
+elif x == 3:
+    print('3 is true')
+elif x == 4:
+    print('4 is true')
+elif x == 5:
+    print('5 is true')
+else:
+    print('none is true')
