6e7776a73971b0d4206c4e4e2d21abdece6f9487
mohamed amr <mohamed.amr@asset.com.eg>
2020-01-23 17:34:34 +0200

    Conditional assignment in Python (ternary operator). closes #27

---
+++ Conditionals/Ternary operator.py
@@ -0,0 +1,6 @@
+# Conditional assignment in Python (ternary operator)
+# Note: you must have if clause and else clause in ternary operator in python.
+
+hungry = 0
+x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
+print(x)
