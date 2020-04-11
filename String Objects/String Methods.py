d6852fa234e9865e002198105e735df7d44dbe0c
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-04-11 02:42:11 +0200

    Common String Methods. closes #70

---
+++ String Objects/String Methods.py
@@ -0,0 +1,9 @@
+# The Python string object has a number of useful methods.
+
+s1 = 'Hello, World.'
+s2 = s1.upper()
+s3 = 'this is another string'
+
+print(s1 + ' ' + s3)
+print(id(s1))
+print(id(s2))
